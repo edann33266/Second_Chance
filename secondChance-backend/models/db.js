756951f7a9d1aa0f59d751bbeb370f2428006a9a
edann33266 <146095512+edann33266@users.noreply.github.com>
2026-07-29 00:22:07 +0530

    Update db.js

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,28 +1,32 @@
 // db.js
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
-// MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
+// MongoDB connection URL
+const url = process.env.MONGO_URL;
 
+// Database instance
 let dbInstance = null;
-const dbName = `${process.env.MONGO_DB}`;
+
+// Database name
+const dbName = process.env.MONGO_DB;
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    // Return existing connection if already connected
+    if (dbInstance) {
+        return dbInstance;
+    }
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    // Task 2: Connect to the database specified in .env
+    dbInstance = client.db(dbName);
 
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
